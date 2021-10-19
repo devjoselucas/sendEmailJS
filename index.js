16d777563baee590efcd73b8812d9046a2ce0771
Dev José Lucas <jose.holanda@ee.ufcg.edu.br>
2021-10-18 21:25:55 -0300

    new feature

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function sendEmail() {
 
-    Email.send({
-        SecureToken : "b562e5cb-b2a8-4ad3-b65c-a466112eeca7",
-        To : 'prototypefirst99@gmail.com',
-        From : "prototypefirst99@gmail.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
+  navigator.geolocation.getCurrentPosition(console.log,console.log);
+
+  Email.send({
+      SecureToken : "b562e5cb-b2a8-4ad3-b65c-a466112eeca7",
+      To : 'prototypefirst99@gmail.com',
+      From : "prototypefirst99@gmail.com",
+      Subject : "This is the subject",
+      Body : "And this is the body"
+  }).then(
+    message => alert(message)
+  );
 
 }
 
