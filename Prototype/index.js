e312cb457073047a107ead72359e396fb07db37c
Dev José Lucas <jose.holanda@ee.ufcg.edu.br>
2021-10-18 21:15:32 -0300

    Prototipo v1.0

---
+++ Prototype/index.js
@@ -0,0 +1,21 @@
+function sendEmail() {
+
+    Email.send({
+        SecureToken : "b562e5cb-b2a8-4ad3-b65c-a466112eeca7",
+        To : 'prototypefirst99@gmail.com',
+        From : "prototypefirst99@gmail.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+
+}
+
+// 4E9666CB0558F21BED1DA02BFBA9D2BE63BE
+
+// smtp.elasticemail.com
+
+// 2525
+
+// b562e5cb-b2a8-4ad3-b65c-a466112eeca7
