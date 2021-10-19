e4e33b38dc1d36ef83b60aff670f2a4aa3e3cd77
Dev José Lucas <jose.holanda@ee.ufcg.edu.br>
2021-10-18 22:57:58 -0300

    Encontrei um erro de sincronia de codigo. Parei aqui

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
+const coordenadas = [];
+
 function sendEmail() {
 
-  const sucessfulLookUp = (position) => {
-    const {latitude, longitude} = position.coords;
-    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=a0a250da0a8e469c9184ed2cf28013da`)
-    .then(response => response.json())
-    .then(console.log);
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
   };
+  
+  function success(pos) {
+    const crd = pos.coords;
+  
+    coordenadas.push(crd.latitude);
+    coordenadas.push(crd.longitude);
 
-  navigator.geolocation.getCurrentPosition(sucessfulLookUp,console.log);
+    console.log(`${coordenadas[0]} ${coordenadas[1]}`);
 
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+  console.log(`${coordenadas[0]} ${coordenadas[1]}`);
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
+
+  console.log(`${coordenadas[0]} ${coordenadas[1]}`);
+/*
   Email.send({
       SecureToken : "b562e5cb-b2a8-4ad3-b65c-a466112eeca7",
       To : 'prototypefirst99@gmail.com',
       From : "prototypefirst99@gmail.com",
       Subject : "This is the subject",
-      Body : "And this is the body"
+      Body : `Latitude : ${coordenadas[0]} Longitude: ${coordenadas[1]}`
   }).then(
     message => alert(message)
   );
-
+*/
 }
 
 // 4E9666CB0558F21BED1DA02BFBA9D2BE63BE
@@ -28,3 +50,39 @@ function sendEmail() {
 // 2525
 
 // b562e5cb-b2a8-4ad3-b65c-a466112eeca7
+
+/*
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+*/
+
+/*
+
+  const sucessfulLookUp = (position) => {
+    const {latitude, longitude} = position.coords;
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=a0a250da0a8e469c9184ed2cf28013da`)
+    .then(response => response.json())
+    .then(console.log);
+  };
+
+  navigator.geolocation.getCurrentPosition(sucessfulLookUp);
+
+  */
